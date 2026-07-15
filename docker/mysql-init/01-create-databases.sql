47735934d8247d2d3db6f8158e3aeb88acbe3e62
Regera24 <regera241@gmail.com>
2026-07-15 10:59:49 +0700

    Update: Update dockerfile & add db init

---
+++ docker/mysql-init/01-create-databases.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS smms_identity_service;
+CREATE DATABASE IF NOT EXISTS smms_staff_service;
---
+++ docker/postgres-init/01-create-databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE smms_order_service;
+CREATE DATABASE smms_inventory_service;
+CREATE DATABASE smms_customer_service;
+CREATE DATABASE smms_report_service;
